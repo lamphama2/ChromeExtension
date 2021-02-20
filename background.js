b74674137eb0b6cd6ccce8aaf3d4f15c553aa3bc
Lam PHAM <phamlegialama2@gmail.com>
2021-02-20 18:05:04 +0100

    :sparkles: Config for tradingview

--- background.js
+++ background.js
@@ -1,12 +1,18 @@
 setTimeout(() => { 
-	console.log("start")
-	removeById("didomi-host")
-	removeById("ez-cookie-dialog-wrapper")
-	removeById("qc-cmp2-container")
-	removeByClass("sticky below_nav piano-paywall relative")
+	var idList = [
+	"didomi-host", 
+	"ez-cookie-dialog-wrapper", 
+	"qc-cmp2-container"
+	]
+	removeById(idList)
+
+	var classList =  [
+	"sticky below_nav piano-paywall relative", 
+	"toast-positioning-wrapper-1beCTfHO",
+	"toast-positioning-wrapper-1beCTfHO"
+	]
+	removeByClass(classList)
 	actionOnClass("content paywall-abo blurText", e => e.style.filter = "none" )
-	launch( function() { document.getElementsByClassName("content paywall-abo blurText").getElementsByClassName("blurText")[0].filter = "none"; } )
-	console.log("finish")
 }, 1000);
 
 function launch(f) {
@@ -18,17 +24,21 @@ function launch(f) {
 }
 
 
-function removeById(id) {
-	launch( function() { document.getElementById(id).remove(); } )
+function removeById(ids) {
+	Array.prototype.forEach.call (ids, id => launch( function() { document.getElementById(id).remove(); }) )			
 }
 
 function removeByClass(cls) {
-	launch( function() { 
-		var list = document.getElementsByClassName(cls)
-		Array.prototype.forEach.call(list, function(element) {
-			element.remove()
-		});
-	} )
+	Array.prototype.forEach.call(cls, function(cl) {
+		launch( function() { 
+			var list = document.getElementsByClassName(cl)
+			Array.prototype.forEach.call(list, function(element) {
+				element.remove()
+			});
+		} )
+
+	})
+
 }
 
 function actionOnClass(cls, f) {
